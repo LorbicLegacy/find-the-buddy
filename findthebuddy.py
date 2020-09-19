1d8c2f741a6f7fdd8e4f3a7462e1610498f40c22
Vikas Patel <vikaspatelp83@gmail.com>
2020-09-19 13:07:47 +0530

    Put websites in a different file

--- findthebuddy.py
+++ findthebuddy.py
@@ -6,8 +6,11 @@ f = open("websites.json")
 webs = json.load(f)
 print(webs["social"])
 print(len(webs))
-websitesList = webs["social"]
+
 user = "narendramodi"
-for w in websitesList:
-    r = web.get(w + user)
-    print(w, " : ", "Found" if str(r) == "<Response [200]>" else "Not Found")
+for websitesName in webs:
+    websitesList = webs[websitesName]
+    print(websitesList)
+    for w in websitesList:
+        r = web.get(w + user)
+        print(w, " : ", "Found" if str(r) == "<Response [200]>" else "Not Found")
